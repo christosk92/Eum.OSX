104cc1417909882efdee508f5d439602c28c774d
Christos Karapasias <christos@marteco.nl>
2022-12-06 16:20:01 +0900

    initial files

---
+++ eumosx/eumosx/eumosxApp.swift
@@ -0,0 +1,17 @@
+//
+//  eumosxApp.swift
+//  eumosx
+//
+//  Created by Christos Karapasias on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct eumosxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
